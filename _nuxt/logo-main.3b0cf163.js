3211f229797302608924b84272731aa724f6d772
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2024-08-28 22:59:17 +0500

    fix

---
+++ _nuxt/logo-main.3b0cf163.js
@@ -0,0 +1,1 @@
+import"./entry.4cf52b94.js";const o=""+globalThis.__publicAssetsURL("svg/logo-shape.svg"),_=""+globalThis.__publicAssetsURL("svg/logo-main.svg");export{o as _,_ as a};
--- _nuxt/logo-main.435f3d90.js
+++
@@ -1,1 +0,0 @@
-import"./entry.014be120.js";const o=""+globalThis.__publicAssetsURL("svg/logo-shape.svg"),_=""+globalThis.__publicAssetsURL("svg/logo-main.svg");export{o as _,_ as a};
